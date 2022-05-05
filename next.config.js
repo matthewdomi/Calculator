d7a80d69d173b14518d5747810a92256fac90076
Michaelbeta <56772706+michaelbeta@users.noreply.github.com>
2022-05-05 15:02:44 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/michaelbeta/CalculadoraNextJS/gh-pages/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/michaelbeta/calculadora_nextjs/gh-pages/' : '',
   
 }
 module.exports=nextConfig
