a8f0b66e4656276f1e8a800c1fd35ac9514efc61
michaelbeta <betancourtmoran@gmail.com>
2022-05-11 21:59:03 -0600

    Aliniar titulo

--- components/layout/HeaderComponent.jsx
+++ components/layout/HeaderComponent.jsx
@@ -17,11 +17,7 @@ const CreateHeaderComponent = () => {
               />
             </a>
           </Link>
-
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon "></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div>
             <h1 className="h1 text-light">Calculadora online sencilla - Nextjs</h1>
           </div>
         </div>
